f19ecde7a964ec60885eb0a18af0d8ee54b90723
ariesvenus <kampoengwisatawebid@gmail.com>
2017-10-18 05:19:41 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$('document').ready(function() {
+	var options = { videoId: 'RyEn3QLb61U', start: 3 };
+	$('#wrapper').tubular(options);
+	// yLeQAS5xSAg sakitnya tuh disiniiii
+	// 1opp3AiTGdU Goyang dumang mannngggggg
+});
